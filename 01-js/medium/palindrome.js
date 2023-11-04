e21b4e7a54fb5af7cd3e91c56533b072c8f6b46d
Yash Rai <82558647+yashrai652@users.noreply.github.com>
2023-11-04 11:20:30 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,16 @@
 */
 
 function isPalindrome(str) {
+ var str1=str.replace(/[\W_]/g, '').toLowerCase();
+  var arr=[];
+  for(var i =str1.length-1;i>=0;i--){
+      arr.push(str1[i]);
+  }
+  var string1 = arr.join('');
+  if(string1==str1)
   return true;
+  else 
+  return false;
 }
 
 module.exports = isPalindrome;
